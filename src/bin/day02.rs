3f51da1ddf7f089c3179dcba5707874581cf2d59
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2020-12-03 13:41:20 +0100

    Improve day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,16 +1,40 @@
 use std::fs;
 
+struct PasswordData {
+    min: usize,
+    max: usize,
+    ch: char,
+    pw: String,
+}
+
+impl PasswordData {
+    fn parse_input(s: &str) -> Option<PasswordData> {
+        let mut token: Vec<&str> = s.split_whitespace().collect();
+
+        let pw = token.pop()?.to_string();
+        let ch = token.pop()?.chars().nth(0)?;
+        let minmax: Vec<&str> = token.pop()?.split('-').collect();
+
+        let min_fromstr = minmax[0].parse().ok()?;
+        let max_fromstr = minmax[1].parse().ok()?;
+
+        Some(PasswordData {
+            min: min_fromstr,
+            max: max_fromstr,
+            ch,
+            pw,
+        })
+    }
+}
+
 struct Solution;
-
-type PasswordData = (usize, usize, char, String);
-
 impl Solution {
     fn part1(pw_data: &Vec<PasswordData>) -> usize {
         pw_data
             .iter()
-            .filter(|(min, max, ch, pw)| {
-                let count = pw.chars().filter(|c| c == ch).count();
-                count >= *min && count <= *max
+            .filter(|pdata| {
+                let count = pdata.pw.chars().filter(|&c| c == pdata.ch).count();
+                count >= pdata.min && count <= pdata.max
             })
             .count()
     }
@@ -18,32 +42,21 @@ impl Solution {
     fn part2(pw_data: &Vec<PasswordData>) -> usize {
         pw_data
             .iter()
-            .map(|(min, max, ch, pw)| (min, max, ch, pw.chars().collect::<Vec<char>>()))
-            .filter(|(&min, &max, &ch, pw)| (pw[min - 1] == ch) ^ (pw[max - 1] == ch))
+            .filter(|pdata| {
+                (pdata.pw.chars().nth(pdata.min - 1).unwrap() == pdata.ch)
+                    ^ (pdata.pw.chars().nth(pdata.max - 1).unwrap() == pdata.ch)
+            })
             .count()
     }
 }
 
 fn main() {
     let input: String = fs::read_to_string("./input/day02.txt").unwrap();
-    let lines: Vec<&str> = input.lines().collect();
 
-    let mut pw_data: Vec<PasswordData> = vec![];
-
-    for l in lines.iter() {
-        let mut token: Vec<&str> = l.split_whitespace().collect();
-
-        let pw = token.pop().unwrap();
-        let ch = token.pop().unwrap().chars().nth(0).unwrap();
-        let minmax: Vec<&str> = token.pop().unwrap().split('-').collect();
-
-        pw_data.push((
-            minmax[0].parse().unwrap(),
-            minmax[1].parse().unwrap(),
-            ch,
-            pw.to_string(),
-        ));
-    }
+    let pw_data: Vec<PasswordData> = input
+        .lines()
+        .map(|l| PasswordData::parse_input(l).unwrap())
+        .collect();
 
     println!("p1: {}", Solution::part1(&pw_data));
     println!("p2: {}", Solution::part2(&pw_data));
