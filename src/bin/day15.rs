16d5db3eddad4aa90799e48b9311df02e75477d8
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2020-12-15 21:08:06 +0100

    Add additonal vector based solution

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,23 +1,23 @@
 use std::collections::HashMap;
 use std::time::Instant;
 
-fn parse(input: &str) -> Vec<u32> {
+fn parse(input: &str) -> Vec<usize> {
     input.split(',').map(|n| n.parse().ok()).flatten().collect()
 }
 
 struct Solution;
 
 impl Solution {
-    fn solve(starting_numbers: &[u32], target: u32) -> u32 {
-        let mut spoken: HashMap<u32, (u32, u32)> = HashMap::new();
+    fn solve_with_hashmap(starting_numbers: &[usize], target: usize) -> usize {
+        let mut spoken: HashMap<usize, (usize, usize)> = HashMap::new();
         let starting_turns = starting_numbers.len();
 
         for (i, num) in starting_numbers.iter().enumerate() {
-            spoken.insert(*num, (i as u32 + 1, 0));
+            spoken.insert(*num, (i + 1, 0));
         }
         let mut last_spoken = starting_numbers[starting_turns - 1];
 
-        for turn in starting_turns as u32 + 1..=target {
+        for turn in starting_turns + 1..=target {
             match spoken[&last_spoken] {
                 (last, sec_last) if sec_last != 0 => {
                     last_spoken = last - sec_last;
@@ -32,6 +32,32 @@ impl Solution {
         }
         last_spoken
     }
+
+    fn solve_with_vector(starting_numbers: &[usize], target: usize) -> usize {
+        let mut spoken: Vec<(usize, usize)> = vec![(0, 0); target + 1];
+        let starting_turns = starting_numbers.len();
+
+        for (i, &num) in starting_numbers.iter().enumerate() {
+            spoken[num] = (i + 1, 0);
+        }
+        let mut last_spoken = starting_numbers[starting_turns - 1];
+
+        for turn in starting_turns + 1..=target {
+            match spoken[last_spoken] {
+                (last, sec_last) if sec_last != 0 => {
+                    last_spoken = last - sec_last;
+                }
+                _ => last_spoken = 0,
+            };
+            let (last, _) = spoken[last_spoken];
+            if last != 0 {
+                spoken[last_spoken] = (turn, last);
+            } else {
+                spoken[last_spoken] = (turn, 0);
+            }
+        }
+        last_spoken
+    }
 }
 
 fn main() {
@@ -40,14 +66,26 @@ fn main() {
 
     let timer = Instant::now();
     println!(
-        "p1: {} (runtime: {:?})",
-        Solution::solve(&starting_numbers, 2020),
+        "p1: {} (HashMap, runtime: {:?})",
+        Solution::solve_with_hashmap(&starting_numbers, 2020),
         timer.elapsed()
     );
     let timer = Instant::now();
     println!(
-        "p2: {} (runtime: {:?})",
-        Solution::solve(&starting_numbers, 30_000_000),
+        "p1: {} (Vec, runtime: {:?})",
+        Solution::solve_with_vector(&starting_numbers, 2020),
+        timer.elapsed()
+    );
+    let timer = Instant::now();
+    println!(
+        "p2: {} (HashMap, runtime: {:?})",
+        Solution::solve_with_hashmap(&starting_numbers, 30_000_000),
+        timer.elapsed()
+    );
+    let timer = Instant::now();
+    println!(
+        "p2: {} (Vec, runtime: {:?})",
+        Solution::solve_with_vector(&starting_numbers, 30_000_000),
         timer.elapsed()
     );
 }
@@ -59,6 +97,7 @@ mod tests {
     #[test]
     fn test_day15_part1() {
         let input = "0,3,6";
-        assert_eq!(Solution::solve(&parse(&input), 2020), 436);
+        assert_eq!(Solution::solve_with_hashmap(&parse(&input), 2020), 436);
+        assert_eq!(Solution::solve_with_vector(&parse(&input), 2020), 436);
     }
 }
