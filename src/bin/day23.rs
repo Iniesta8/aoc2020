80c059be74d3a9987dc5dc78e8f7913a0940447c
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2020-12-23 17:53:35 +0100

    Remove slow solution

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -9,61 +9,6 @@ fn parse(input: &str) -> Vec<usize> {
         .collect()
 }
 
-fn _play_p1_slowly(cups: Vec<usize>) -> Vec<usize> {
-    let mut cups = cups;
-    let mut current_cup = 0;
-
-    for i in 0..100 {
-        println!("-- move {} --", i + 1);
-        println!("cups: {:?}", cups);
-
-        let current_cup_label = cups[current_cup];
-        let mut pick_up: Vec<usize> = vec![];
-        let mut pick_pos = current_cup + 1;
-        for _ in 0..3 {
-            if pick_pos < cups.len() {
-                pick_up.push(cups.remove(pick_pos));
-            } else {
-                pick_pos = 0;
-                pick_up.push(cups.remove(pick_pos));
-            }
-        }
-
-        let lowest_cup = *cups.iter().min().unwrap();
-        let highest_cup = *cups.iter().max().unwrap();
-        let mut destination_cup = current_cup_label - 1;
-
-        println!("current cup label: {}", current_cup_label);
-        println!("pick up: {:?}", pick_up);
-
-        if destination_cup < lowest_cup {
-            destination_cup = highest_cup;
-        }
-
-        while pick_up.contains(&destination_cup) {
-            destination_cup -= 1;
-            if destination_cup < lowest_cup {
-                destination_cup = highest_cup;
-            }
-        }
-        println!("destination: {}", destination_cup);
-
-        let new_pos = (cups.iter().position(|&c| c == destination_cup).unwrap() + 1) % cups.len();
-
-        if new_pos >= cups.len() {
-            cups.append(&mut pick_up);
-        } else {
-            for j in 0..pick_up.len() {
-                cups.insert(new_pos + j, pick_up[j]);
-            }
-        }
-
-        current_cup = (cups.iter().position(|&c| c == current_cup_label).unwrap() + 1) % cups.len();
-    }
-
-    cups
-}
-
 fn list_to_vec(cups: &[usize], current_cup: usize) -> Vec<usize> {
     let mut res = vec![];
     let mut ptr = current_cup;
