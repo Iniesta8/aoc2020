100d27a3932ddb9b05623513e6ebe2bff47fbe9a
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2020-12-24 13:32:44 +0100

    Add day24 part2

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -2,22 +2,21 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
-#[derive(PartialEq)]
+#[derive(Copy, Clone, PartialEq)]
 enum Color {
     White,
     Black,
 }
 
-// Hexgrid coordinates
-//       -z
-//  +y        +x
-//        .
-//  -x        -y
-//       +z
+// hexgrid coordinates
+//      -z
+//  +y      +x
+//       X
+//  -x      -y
+//      +z
+type Position = (i32, i32, i32); // x, y, z
 
-type Tile = (i32, i32, i32); // x, y, z
-
-fn parse(input: &str) -> HashMap<Tile, Color> {
+fn parse(input: &str) -> HashMap<Position, Color> {
     let mut hexgrid = HashMap::new();
 
     hexgrid.insert((0, 0, 0), Color::White);
@@ -81,7 +80,6 @@ fn parse(input: &str) -> HashMap<Tile, Color> {
         }
 
         assert_eq!(x + y + z, 0);
-        println!("identified tile: ({}, {}, {})", x, y, z);
 
         let current_entry = hexgrid.entry((x, y, z)).or_insert(Color::White);
 
@@ -95,22 +93,126 @@ fn parse(input: &str) -> HashMap<Tile, Color> {
     hexgrid
 }
 
+fn count_black_tiles(hexgrid: &HashMap<Position, Color>) -> usize {
+    hexgrid.values().filter(|&c| *c == Color::Black).count()
+}
+
+fn get_adjacent_tiles(
+    hexgrid: &HashMap<Position, Color>,
+    from: &Position,
+) -> HashMap<Position, Color> {
+    let adj_directions = [
+        (1, -1, 0),
+        (0, -1, 1),
+        (-1, 0, 1),
+        (-1, 1, 0),
+        (0, 1, -1),
+        (1, 0, -1),
+    ];
+
+    let (x, y, z) = from;
+    let mut adjacent_tiles = HashMap::new();
+
+    for dir in adj_directions.iter() {
+        let (xi, yi, zi) = dir;
+        let adj_pos = (x + xi, y + yi, z + zi);
+
+        adjacent_tiles.insert(adj_pos, {
+            match hexgrid.get(&adj_pos) {
+                None => Color::White,
+                Some(&color) => color,
+            }
+        });
+    }
+
+    assert_eq!(adjacent_tiles.len(), 6);
+    adjacent_tiles
+}
+
+fn calc_new_tile_color(color: Color, num_adjacent_black_tiles: usize) -> Color {
+    assert!(num_adjacent_black_tiles <= 6);
+
+    match color {
+        Color::Black
+            if num_adjacent_black_tiles == 0
+                || (3..=6).into_iter().any(|c| c == num_adjacent_black_tiles) =>
+        {
+            Color::White
+        }
+
+        Color::White if num_adjacent_black_tiles == 2 => Color::Black,
+        _ => color,
+    }
+}
+
+fn process_tile(
+    cur_grid: &HashMap<Position, Color>,
+    next_grid: &mut HashMap<Position, Color>,
+    tile: (&Position, &Color),
+) -> HashMap<Position, Color> {
+    let (tile_pos, tile_color) = tile;
+
+    let adj_tiles = get_adjacent_tiles(&cur_grid, &tile_pos);
+    let num_adj_black_tiles = count_black_tiles(&adj_tiles);
+    let new_tile_color = calc_new_tile_color(*tile_color, num_adj_black_tiles);
+
+    if new_tile_color == Color::Black {
+        next_grid.insert(*tile_pos, Color::Black);
+    }
+
+    adj_tiles
+}
+
+fn flip_tiles(hexgrid: &HashMap<Position, Color>, cycles: usize) -> HashMap<Position, Color> {
+    let mut cur_grid = hexgrid.clone();
+
+    for _ in 0..cycles {
+        let mut next_grid = HashMap::new();
+        for tile in cur_grid.iter() {
+            let adj_tiles = process_tile(&cur_grid, &mut next_grid, tile);
+            let ext_tiles: HashMap<_, Color> = adj_tiles
+                .into_iter()
+                .filter(|(tile_pos, _)| !cur_grid.contains_key(&tile_pos))
+                .collect();
+
+            for ext_tile in ext_tiles.iter() {
+                process_tile(&cur_grid, &mut next_grid, ext_tile);
+            }
+        }
+        cur_grid = next_grid.clone();
+    }
+
+    cur_grid
+}
+
 struct Solution;
 
 impl Solution {
-    fn part1(hexgrid: &HashMap<Tile, Color>) -> usize {
-        hexgrid.values().filter(|&c| *c == Color::Black).count()
+    fn part1(hexgrid: &HashMap<Position, Color>) -> usize {
+        count_black_tiles(&hexgrid)
+    }
+
+    fn part2(hexgrid: &HashMap<Position, Color>) -> usize {
+        let final_hexgrid = flip_tiles(hexgrid, 100);
+        count_black_tiles(&final_hexgrid)
     }
 }
 
 fn main() {
     let input = fs::read_to_string("./input/day24.txt").expect("File not found!");
-    let hexgrid: HashMap<Tile, Color> = parse(&input);
+    let hexgrid: HashMap<Position, Color> = parse(&input);
 
     let timer = Instant::now();
     println!(
         "p1: {} (runtime: {:?})",
         Solution::part1(&hexgrid),
+        timer.elapsed()
+    );
+
+    let timer = Instant::now();
+    println!(
+        "p2: {} (runtime: {:?})",
+        Solution::part2(&hexgrid),
         timer.elapsed()
     );
 }
@@ -142,7 +244,8 @@ nenewswnwewswnenesenwnesewesw
 eneswnwswnwsenenwnwnwwseeswneewsenese
 neswnwewnwnwseenwseesewsenwsweewe
 wseweeenwnesenwwwswnew";
-        let hexgrid: HashMap<Tile, Color> = parse(&input);
+        let hexgrid: HashMap<Position, Color> = parse(&input);
         assert_eq!(Solution::part1(&hexgrid), 10);
+        assert_eq!(Solution::part2(&hexgrid), 2208);
     }
 }
